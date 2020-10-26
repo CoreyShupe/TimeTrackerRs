4c53b5e7610fc422c52ebd745616b1e9f02d206d
CoreyShupe <coreyshupe101@gmail.com>
2020-10-26 01:36:26 -0400

    First version implementation for druid gui support.

---
+++ old_src/main.rs
@@ -0,0 +1,105 @@
+extern crate prettytable;
+extern crate clap;
+
+mod tracker;
+
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use clap::{App, AppSettings, Arg, SubCommand};
+use crate::tracker::{Tracker, TrackEntry};
+
+const CLEAR_CMD_VERSION: &str = "0.0.1";
+const TRACK_CMD_VERSION: &str = "0.0.2";
+const SHOW_CMD_VERSION: &str = "0.0.2";
+
+fn main() {
+    let app_matcher = App::new("Time Tracker")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg(Arg::with_name("path")
+            .index(1)
+            .short("p")
+            .required(true)
+            .help("The file to contain and pull the tracker info for."))
+        .subcommand(base_sub_command("clear", "Clears tracker information.", CLEAR_CMD_VERSION).alias("c"))
+        .subcommand(base_sub_command("track", "Begins the time tracker.", TRACK_CMD_VERSION)
+            .arg(Arg::with_name("description")
+                .index(1)
+                .short("d")
+                .required(true)
+                .help("Description of what's being tracked."))
+        )
+        .subcommand(base_sub_command("show", "Shows the time tracked.", SHOW_CMD_VERSION).alias("s"))
+        .get_matches();
+
+    let path = Path::new(app_matcher.value_of("path").unwrap()).to_path_buf();
+
+    // executions
+
+    match app_matcher.subcommand_name() {
+        Some("clear") => {
+            if path.exists() {
+                std::fs::remove_file(path).expect("Failed to remove file.");
+            }
+            println!("Your tracking progress has been cleared.")
+        }
+        Some("track") => {
+            print!("Your tracker has started, type anything to stop the tracker: ");
+
+            let description = app_matcher.subcommand_matches("track").unwrap()
+                .value_of("description").unwrap();
+
+            let current = get_current_ms();
+
+            // block until typed
+            stdout().flush().expect("Failed to flush stdout.");
+            stdin().read_line(&mut String::new()).expect("Did not enter a proper string.");
+            // end blocking
+
+            let after = get_current_ms();
+
+            let mut tracker = if *&path.exists() {
+                Tracker::read_from(&path).expect("Failed to read CSV file.")
+            } else {
+                Tracker::new()
+            };
+            tracker = tracker.insert_entry(TrackEntry::new(
+                get_current_ms(),
+                after - current,
+                String::from(description),
+            ));
+            tracker.write_to(&path).expect("Failed to write CSV file.");
+
+            println!("You have successfully tracked {} time for {}.", tracker::ms_to_time(after - current), description);
+        }
+        Some("show") => {
+            if !path.exists() {
+                println!("You have no time currently logged.");
+                return;
+            }
+
+            let tracker = Tracker::read_from(&path).expect("Failed to read CSV file.");
+            tracker.gen_table().printstd();
+        }
+        _ => unreachable!()
+    }
+}
+
+// sub command utilities
+
+fn base_sub_command<'a, 'b, S: Into<&'b str>>(name: S, about: S, version: S) -> App<'a, 'b> {
+    SubCommand::<'a>::with_name(name.into())
+        .about(about)
+        .version(version)
+        .author("Corey Shupe")
+}
+
+// time utilities
+
+fn get_current_ms() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time is not working properly.")
+        .as_millis()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,215 @@
-extern crate prettytable;
-extern crate clap;
+use druid::{
+    im::{Vector, HashMap},
+    widget::{Button, CrossAxisAlignment, Flex, FlexParams, Label, List, Padding, Scroll, TextBox},
+    AppLauncher, Data, Lens, PlatformError, UnitPoint, Widget, WidgetExt, WindowDesc,
+};
+use std::{
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
-mod tracker;
+#[derive(Clone, Data)]
+struct TrackerEntry {
+    entry_date: Arc<u128>,
+    time_spent_ms: Arc<u128>,
+    description: String,
+}
 
-use std::io::{stdin, stdout, Write};
-use std::path::Path;
-use std::time::{SystemTime, UNIX_EPOCH};
+#[derive(Clone, Data)]
+struct TrackerSection {
+    time_spent_ms: Arc<u128>,
+    description: String,
+}
 
-use clap::{App, AppSettings, Arg, SubCommand};
-use crate::tracker::{Tracker, TrackEntry};
+#[derive(Clone, Data, Lens)]
+struct TrackerInfo {
+    tracker_start: Arc<u128>,
+    description: String,
+}
 
-const CLEAR_CMD_VERSION: &str = "0.0.1";
-const TRACK_CMD_VERSION: &str = "0.0.2";
-const SHOW_CMD_VERSION: &str = "0.0.2";
+#[derive(Clone, Data, Lens)]
+struct TrackerEntryData {
+    storage: Vector<TrackerEntry>,
+    section_storage: Vector<TrackerSection>,
+    current_entry: Option<TrackerInfo>,
+    incoming_descriptor: String,
+}
 
-fn main() {
-    let app_matcher = App::new("Time Tracker")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .arg(Arg::with_name("path")
-            .index(1)
-            .short("p")
-            .required(true)
-            .help("The file to contain and pull the tracker info for."))
-        .subcommand(base_sub_command("clear", "Clears tracker information.", CLEAR_CMD_VERSION).alias("c"))
-        .subcommand(base_sub_command("track", "Begins the time tracker.", TRACK_CMD_VERSION)
-            .arg(Arg::with_name("description")
-                .index(1)
-                .short("d")
-                .required(true)
-                .help("Description of what's being tracked."))
-        )
-        .subcommand(base_sub_command("show", "Shows the time tracked.", SHOW_CMD_VERSION).alias("s"))
-        .get_matches();
+impl TrackerEntryData {
+    fn end_entry_calculation(&mut self, tracker_info: &TrackerInfo) {
+        if tracker_info.description.is_empty() {
+            self.current_entry = Option::None;
+            return;
+        }
+        let time = get_current_ms();
+        let time_spent = time - *tracker_info.tracker_start.clone();
+        if time_spent == 0 {
+            self.current_entry = Option::None;
+            return;
+        }
+        self.storage.push_back(TrackerEntry {
+            entry_date: Arc::new(time),
+            time_spent_ms: Arc::new(time_spent),
+            description: tracker_info.description.clone(),
+        });
+        self.current_entry = Option::None;
+        self.parse_tracker_sections();
+    }
 
-    let path = Path::new(app_matcher.value_of("path").unwrap()).to_path_buf();
+    fn parse_tracker_sections(&mut self) {
+        let mut section_map = HashMap::<String, u128>::new();
 
-    // executions
+        for tracker_entry in &self.storage {
+            let descriptor = tracker_entry.description.clone();
+            let initial = *section_map.get(&descriptor).unwrap_or(&0u128);
+            let incrementer = *tracker_entry.time_spent_ms.clone();
+            section_map.insert(descriptor, initial + incrementer);
+        }
 
-    match app_matcher.subcommand_name() {
-        Some("clear") => {
-            if path.exists() {
-                std::fs::remove_file(path).expect("Failed to remove file.");
+        let mut new_storage = Vector::new();
+
+        for entry in section_map {
+            new_storage.push_back(TrackerSection {
+                time_spent_ms: Arc::new(entry.1),
+                description: entry.0,
+            });
+        }
+
+        self.section_storage = new_storage;
+        self.section_storage.sort_by(|o1, o2| o1.description.cmp(&o2.description))
+    }
+
+    fn start_entry_calculation(&mut self) {
+        if self.incoming_descriptor.is_empty() {
+            return;
+        }
+        self.current_entry = Option::Some(TrackerInfo {
+            tracker_start: Arc::new(get_current_ms()),
+            description: self.incoming_descriptor.clone(),
+        })
+    }
+
+    fn swap_states(&mut self) {
+        let current_info = {
+            &self.current_entry.clone()
+        };
+
+        match current_info {
+            Some(tracker_info) => {
+                &self.end_entry_calculation(tracker_info);
             }
-            println!("Your tracking progress has been cleared.")
-        }
-        Some("track") => {
-            print!("Your tracker has started, type anything to stop the tracker: ");
-
-            let description = app_matcher.subcommand_matches("track").unwrap()
-                .value_of("description").unwrap();
-
-            let current = get_current_ms();
-
-            // block until typed
-            stdout().flush().expect("Failed to flush stdout.");
-            stdin().read_line(&mut String::new()).expect("Did not enter a proper string.");
-            // end blocking
-
-            let after = get_current_ms();
-
-            let mut tracker = if *&path.exists() {
-                Tracker::read_from(&path).expect("Failed to read CSV file.")
-            } else {
-                Tracker::new()
-            };
-            tracker = tracker.insert_entry(TrackEntry::new(
-                get_current_ms(),
-                after - current,
-                String::from(description),
-            ));
-            tracker.write_to(&path).expect("Failed to write CSV file.");
-
-            println!("You have successfully tracked {} time for {}.", tracker::ms_to_time(after - current), description);
-        }
-        Some("show") => {
-            if !path.exists() {
-                println!("You have no time currently logged.");
-                return;
+            None => {
+                &self.start_entry_calculation();
             }
-
-            let tracker = Tracker::read_from(&path).expect("Failed to read CSV file.");
-            tracker.gen_table().printstd();
-        }
-        _ => unreachable!()
+        };
     }
 }
 
-// sub command utilities
+fn build_ui() -> impl Widget<TrackerEntryData> {
+    let description_text_box = TextBox::new()
+        .lens(TrackerEntryData::incoming_descriptor)
+        .expand_width();
 
-fn base_sub_command<'a, 'b, S: Into<&'b str>>(name: S, about: S, version: S) -> App<'a, 'b> {
-    SubCommand::<'a>::with_name(name.into())
-        .about(about)
-        .version(version)
-        .author("Corey Shupe")
+    let switch_button = Button::new(|data: &Option<TrackerInfo>, _env: &_| {
+        match data {
+            Some(_) => String::from("Stop"),
+            None => String::from("Start")
+        }
+    }).lens(TrackerEntryData::current_entry).on_click(move |_event, data: &mut TrackerEntryData, _env| {
+        data.swap_states();
+    });
+
+    let output_text = Label::new(|data: &Option<TrackerInfo>, _env: &_| {
+        match data {
+            Some(tracker_info) => {
+                format!("Tracker Running: `{}`", &tracker_info.description)
+            }
+            None => {
+                String::from("Tracker Not Started")
+            }
+        }
+    }).lens(TrackerEntryData::current_entry);
+
+    let scroll_list = Scroll::new(List::new(|| {
+        Label::new(|item: &TrackerSection, _env: &_| {
+            format!("| {} | {} |", item.description, ms_to_time(*item.time_spent_ms))
+        }).align_vertical(UnitPoint::LEFT).padding(1.0).expand_width()
+    })).vertical().lens(TrackerEntryData::section_storage);
+
+    Padding::new(3.0, Flex::column()
+        .with_flex_child(
+            Flex::row()
+                .with_flex_child(description_text_box, 1.0)
+                .with_flex_spacer(0.1)
+                .with_flex_child(switch_button, 1.0)
+                .with_flex_spacer(0.1)
+                .with_flex_child(output_text, 1.0),
+            1.0)
+        .with_flex_spacer(0.2)
+        .with_flex_child(
+            Label::new("| Description | Time Spent |"),
+            FlexParams::new(1.0, CrossAxisAlignment::Start))
+        .with_flex_spacer(0.05)
+        .with_flex_child(scroll_list, 1.0),
+    )
 }
 
-// time utilities
+fn main() -> Result<(), PlatformError> {
+    AppLauncher::with_window(WindowDesc::new(build_ui)).launch(TrackerEntryData {
+        storage: Vector::new(),
+        section_storage: Vector::new(),
+        current_entry: Option::None,
+        incoming_descriptor: String::from(""),
+    })?;
+
+    Ok(())
+}
 
 fn get_current_ms() -> u128 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time is not working properly.")
         .as_millis()
+}
+
+pub fn ms_to_time(time: u128) -> String {
+    let mut seconds = time / 1000;
+    let mut minutes = seconds / 60;
+    seconds %= 60;
+    let hours = minutes / 60;
+    minutes %= 60;
+
+    let mut display = String::new();
+    let mut flag = false;
+
+    if hours > 0 {
+        display += format!("{} Hour", hours).as_str();
+        if hours > 1 {
+            display += "s";
+        }
+        flag = true;
+    }
+
+    if minutes > 0 {
+        if flag {
+            display += " ";
+        }
+        display += format!("{} Minute", minutes).as_str();
+        if minutes > 1 {
+            display += "s";
+        }
+        flag = true;
+    }
+
+    if seconds > 0 {
+        if flag {
+            display += " ";
+        }
+        display += format!("{} Second", seconds).as_str();
+        if seconds > 1 {
+            display += "s";
+        }
+    }
+
+    display
 }
